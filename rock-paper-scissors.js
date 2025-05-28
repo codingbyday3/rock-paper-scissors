e7a0aa070bd6f494286b9cb192066a633371572c
Klemen <klemen.bussines12@gmail.com>
2025-05-28 20:17:25 +0200

    Build: build function which return user input

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,9 +1,16 @@
 
 function getComputerChoice(){
-      randomNum = Math.floor(Math.random() * 3)
+  randomNum = Math.floor(Math.random() * 3)
       
-      plays = ["rock", "paper", "scissors"]
+  plays = ["rock", "paper", "scissors"]
 
-      return plays[randomNum]
-    }
-    // console.log(getComputerChoice())
+  return plays[randomNum]
+}
+// console.log(getComputerChoice())
+
+function getHumanChoice(){
+  userPlay = prompt("What is your move?")
+
+  return userPlay
+}
+console.log(getHumanChoice())
